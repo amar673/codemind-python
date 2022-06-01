cf730f24fd1e1797b44f778c00070343bb7af74b
amar673 <102217540+amar673@users.noreply.github.com>
2022-06-01 15:11:46 +0530

    def ugl(n):     while n%2==0:         n=n//2     if n==1:         return True     while n%3==0:         n=n//3     if n==1:         return True     while n%5==0:         n=n//5     if n==1:         return True     return False n=int(input()) if ugl(n):     print("Ugly Number") else:     print("Not Ugly Number")

---
+++ Ugly_number.py
@@ -0,0 +1,22 @@
+def ugl(n):
+    while n%2==0:
+        n=n//2
+    if n==1:
+        return True
+    while n%3==0:
+        n=n//3
+    if n==1:
+        return True
+    while n%5==0:
+        n=n//5
+    if n==1:
+        return True
+    return False
+n=int(input())
+if ugl(n):
+    print("Ugly Number")
+else:
+    print("Not Ugly Number")
+    
+        
+    
