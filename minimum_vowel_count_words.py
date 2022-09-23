1306e9dc8eb9cac0688bb241da078e44f6a6e167
amar673 <102217540+amar673@users.noreply.github.com>
2022-09-23 12:11:40 +0530

    s=input() s=s.split() l=[] c1=0 v="aeiouAEIOU" for i in s:     c=0     for j in i:         if j in v:             c+=1     l.append(c) for i in l:     if i==min(l):         c1+=1 print(c1)

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,15 @@
+s=input()
+s=s.split()
+l=[]
+c1=0
+v="aeiouAEIOU"
+for i in s:
+    c=0
+    for j in i:
+        if j in v:
+            c+=1
+    l.append(c)
+for i in l:
+    if i==min(l):
+        c1+=1
+print(c1)
